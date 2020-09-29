00911898eafbfb2c360d0b1cdadc3f31c334a8e7
Ronald Sengkey <ronaldsengkey@usahakreatif.co.id>
2020-09-29 17:23:22 +0700

    my commit

---
+++ config/mongo.js
@@ -0,0 +1,6 @@
+const mongoUrl = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DATABASE}?authSource=admin`;
+module.exports = {
+    mongoDb: {
+        url: mongoUrl
+    }
+};
---
+++ index.js
@@ -0,0 +1,53 @@
+"use strict";
+require('dotenv').config();
+var fs = require("fs"),
+  path = require("path"),
+  mongoose = require('mongoose').set('debug', true),
+  mongoConf = require('./config/mongo.js');
+// http = require("http");
+
+var app = require("restana")();
+var swaggerTools = require("swagger-tools");
+var jsyaml = require("js-yaml");
+var serverPort = 8080;
+
+// swaggerRouter configuration
+var options = {
+  swaggerUi: path.join(__dirname, "/swagger.json"),
+  controllers: path.join(__dirname, "./controllers"),
+  useStubs: process.env.NODE_ENV === "development", // Conditionally turn on stubs (mock mode)
+};
+
+// The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
+var spec = fs.readFileSync(path.join(__dirname, "api/swagger.yaml"), "utf8");
+var swaggerDoc = jsyaml.safeLoad(spec);
+
+// Initialize the Swagger middleware
+swaggerTools.initializeMiddleware(swaggerDoc, async function (middleware) {
+  // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
+  app.use(middleware.swaggerMetadata());
+
+  // Validate Swagger requests
+  app.use(middleware.swaggerValidator());
+
+  // Route validated requests to appropriate controller
+  app.use(middleware.swaggerRouter(options));
+
+  // Serve the Swagger documents and Swagger UI
+  app.use(middleware.swaggerUi());
+
+
+
+  // Start the server
+  app.start(serverPort, "0.0.0.0").then((server) => {});
+
+  mongoose.connect(mongoConf.mongoDb.url, {
+    useUnifiedTopology: true
+  }).then(function (e) {
+    console.log("MONGO CONNECTED");
+  });
+  // http.createServer(app).listen(serverPort, function () {
+  //   console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+  //   console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+  // });
+});
